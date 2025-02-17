67a6e78638fdf498a83824f6b66ea7ff582b28b8
rogergvr <rogervr@gmail.com>
2025-02-16 20:14:19 -0800

    Array Nombres, Agregar Amigos, Actualizar Amigos en desarrollo

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresCandidatos = [];
+let listaCandidatos = document.getElementById('listaAmigos');
+
+function agregarAmigo(){
+    let nuevoAmigo = document.getElementById('amigo').value;
+    //console.log(nuevoAmigo);
+    if (nuevoAmigo == ""){
+        alert('Por favor, ingrese un nombre válido')
+    } else {
+        nombresCandidatos.push(nuevoAmigo);
+        console.log(nombresCandidatos);
+        //limpiarTexto();
+        solicitaNuevoNombre();
+        actualizarListaAmigos();
+    }
+    return;
+}
+
+function actualizarListaAmigos() {
+    let elementoLista = document.createElement("li");
+    listaCandidatos.innerHTML = "";
+    console.log('Limpia Lista HTML');
+    console.log(listaCandidatos);
+    
+    for (let index = 0; index < nombresCandidatos.length; index++) {
+        //console.log(nombresCandidatos[index]);
+        console.log(index);       
+        elementoLista.textContent = nombresCandidatos[index];
+        listaCandidatos.appendChild(elementoLista);
+        console.log(listaCandidatos);       
+        elementoLista = document.createElement("li");
+    }
+        
+    return;
+}
+
+/*
+function actualizarListaAmigos(nuevoAmigo) {
+    let elementoLista = document.createElement("li");
+    elementoLista.textContent = nuevoAmigo
+    listaCandidatos.appendChild(elementoLista);
+    console.log(listaCandidatos);
+    return;
+}
+*/
+
+function limpiarTexto(){
+    document.getElementById('amigo').value='';
+    return;
+}
+
+function solicitaNuevoNombre(){
+    document.getElementById('amigo').value='';
+    document.getElementById('amigo').setAttribute('placeholder', 'Ingresa un nuevo nombre');
+    return;
+}
