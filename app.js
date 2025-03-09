953bd18fc5253f0ac3b54526658939abc3a2cfad
rogergvr <rogervr@gmail.com>
2025-03-08 16:53:08 -0800

    Primera version productiva

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ function agregarAmigo(){
     let nuevoAmigo = document.getElementById('amigo').value;
     //console.log(nuevoAmigo);
     if (nuevoAmigo == ""){
-        alert('Por favor, ingrese un nombre válido')
+        alert('Por favor, ingrese un nombre válido');
     } else {
         nombresCandidatos.push(nuevoAmigo);
-        console.log(nombresCandidatos);
+        //console.log(nombresCandidatos);
         //limpiarTexto();
         solicitaNuevoNombre();
         actualizarListaAmigos();
@@ -20,21 +20,43 @@ function agregarAmigo(){
 function actualizarListaAmigos() {
     let elementoLista = document.createElement("li");
     listaCandidatos.innerHTML = "";
-    console.log('Limpia Lista HTML');
-    console.log(listaCandidatos);
+//    console.log('Limpia Lista HTML');
+//    console.log(listaCandidatos);
     
     for (let index = 0; index < nombresCandidatos.length; index++) {
         //console.log(nombresCandidatos[index]);
-        console.log(index);       
+        //console.log(index);       
         elementoLista.textContent = nombresCandidatos[index];
         listaCandidatos.appendChild(elementoLista);
-        console.log(listaCandidatos);       
+        //console.log(listaCandidatos);       
         elementoLista = document.createElement("li");
     }
-        
+       
     return;
 }
 
+function sortearAmigo() {
+
+    if (validarAmigos()){
+        let indiceAmigo = Math.floor(Math.random()*nombresCandidatos.length);
+        console.log(indiceAmigo);
+        let elementoAmigo = document.createElement("li");
+        listaCandidatos.innerHTML = "";
+        elementoAmigo.textContent = nombresCandidatos[indiceAmigo];
+        listaCandidatos.appendChild(elementoAmigo);
+    }
+    return;
+}
+
+function validarAmigos() {
+    console.log(nombresCandidatos);
+    if (nombresCandidatos.length == 0) {
+        alert('Aún no ha agregado amigos a la lista');
+        return false;
+    } else {
+        return true;
+    }
+}
 /*
 function actualizarListaAmigos(nuevoAmigo) {
     let elementoLista = document.createElement("li");
